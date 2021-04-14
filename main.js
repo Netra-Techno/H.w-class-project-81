2d84eff6bb5b4604a02cbd41b3101543541b23f3
Netra <76477241+Netra-Techno@users.noreply.github.com>
2021-04-14 13:17:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+color-"blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(220, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+color-"black";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(220, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
